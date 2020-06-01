441dc0f904fdabce39b1913d20057a6965c9afe6
kike Martinez <enmarcor@hotmail.com>
2020-06-01 13:45:19 -0500

    react

---
+++ precache-manifest.69f091c5c660b92a787f80aac41dfe30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5261b66838488f24c4892acd99c62cb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af858a5165bd28fafd1",
+    "url": "/static/js/2.1a4cdf84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a4cdf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be71a204d3d524812e74",
+    "url": "/static/js/main.1879817f.chunk.js"
+  },
+  {
+    "revision": "e275a57e96d358f64ebf",
+    "url": "/static/js/runtime-main.6dfc73c8.js"
+  }
+]);
